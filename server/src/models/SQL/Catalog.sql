9053acd4a904389f7741dec46438e3e01bea6f30
Rural Vladislav <dolgkozly@gmail.com>
2023-08-28 14:44:05 +0300

    Remake SQL catalog

--- server/src/models/SQL/Catalog.sql
+++ server/src/models/SQL/Catalog.sql
@@ -5,3 +5,11 @@ CREATE TABLE Catalog {
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
 }
+
+CREATE TABLE CatalogChat{
+  id SERIAL PRIMARY KEY,
+  catalog_id INT NOT NULL,
+  chat_id INT NOT NULL,
+  FOREIGN KEY (catalog_id) REFERENCES Catalog(id),
+  FOREIGN KEY (chat_id) REFERENCES Conversation(id)
+} 
