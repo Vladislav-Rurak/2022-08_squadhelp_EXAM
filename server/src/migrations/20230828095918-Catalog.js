fc88748012b3d398720288bd7bc3fdcdc064f2db
Rural Vladislav <dolgkozly@gmail.com>
2023-08-28 15:12:12 +0300

    screen UML & rename Catalog

--- server/src/migrations/20230828095918-Catalog.js
+++ server/src/migrations/20230828095918-Catalog.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Catalogs', {
+    await queryInterface.createTable('Catalog', {
       id: {
         allowNull: false,
         autoIncrement: true,
