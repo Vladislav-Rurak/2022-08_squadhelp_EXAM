3a146f26a8feccf22a477314f52c214e53b32ca4
Rural Vladislav <dolgkozly@gmail.com>
2023-11-08 10:56:09 +0200

    fixed creation contest with image

--- server/src/middlewares/basicMiddlewares.js
+++ server/src/middlewares/basicMiddlewares.js
@@ -8,9 +8,10 @@ module.exports.parseBody = (req, res, next) => {
   req.body.contests = JSON.parse(req.body.contests)
   for (let i = 0; i < req.body.contests.length; i++) {
     if (req.body.contests[i].haveFile) {
-      const file = req.files.splice(0, 1)
-      req.body.contests[i].fileName = file[0].filename
-      req.body.contests[i].originalFileName = file[0].originalname
+      const cuttingPath = req.body.files
+      const file = cuttingPath.replace(/^.*[\\\/]/, '')
+      req.body.contests[i].fileName = file + Date()
+      req.body.contests[i].originalFileName = file
     }
   }
   next()
