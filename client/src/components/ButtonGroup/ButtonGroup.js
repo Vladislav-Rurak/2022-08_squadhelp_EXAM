0d9529bb3fb749d66b869080f8c75b461062d9d1
Rural Vladislav <dolgkozly@gmail.com>
2023-11-10 13:34:55 +0200

    Separation into components and unuse index element

--- client/src/components/ButtonGroup/ButtonGroup.js
+++ client/src/components/ButtonGroup/ButtonGroup.js
@@ -1,72 +1,49 @@
-import { React, useState } from 'react'
+import React, { useState } from 'react'
+import ButtonComponent from './ButtonComponent'
 import styles from './ButtonGroup.module.sass'
 
 const ButtonGroup = () => {
-  const [texts, setTexts] = useState(['No', 'No', 'No'])
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(-1)
 
-  const textChange = i => {
-    const newTexts = [...texts]
-    if (newTexts[i] === 'No') {
-      newTexts[i] = 'Yes'
-    } else {
-      newTexts[i] = 'No'
-    }
-    setTexts(newTexts)
+  const textChange = index => {
+    setSelectedButtonIndex(index)
   }
+
+  const buttonData = [
+    {
+      text: selectedButtonIndex === 0 ? 'Yes' : 'No',
+      onClick: () => textChange(0),
+      className:
+        selectedButtonIndex === 0
+          ? styles.ststatementButtons
+          : styles.negativeButtons,
+      parText: 'The Domain should exactly match the name'
+    },
+    {
+      text: selectedButtonIndex === 1 ? 'Yes' : 'No',
+      onClick: () => textChange(1),
+      className:
+        selectedButtonIndex === 1
+          ? styles.ststatementButtons
+          : styles.negativeButtons,
+      parText: 'But minor variations are allowed (Recommended)'
+    },
+    {
+      text: selectedButtonIndex === 2 ? 'Yes' : 'No',
+      onClick: () => textChange(2),
+      className:
+        selectedButtonIndex === 2
+          ? styles.ststatementButtons
+          : styles.negativeButtons,
+      parText: 'I am only looking for a name, not a Domain'
+    }
+  ]
 
   return (
     <div className={styles.buttonGroupContainer}>
-      <button
-        onClick={() => textChange(0)}
-        className={
-          texts[0] === 'No' ? styles.negativeButtons : styles.ststatementButtons
-        }
-      >
-        <span
-          className={
-            texts[0] === 'No' ? styles.negativeText : styles.statementText
-          }
-        >
-          {texts[0]}
-        </span>
-        <p className={texts[0] === 'No' ? styles.negativeParText : ' '}>
-          The Domain should exactly match the name
-        </p>
-      </button>
-      <button
-        onClick={() => textChange(1)}
-        className={
-          texts[1] === 'No' ? styles.negativeButtons : styles.ststatementButtons
-        }
-      >
-        <span
-          className={
-            texts[1] === 'No' ? styles.negativeText : styles.statementText
-          }
-        >
-          {texts[1]}
-        </span>
-        <p className={texts[1] === 'No' ? styles.negativeParText : ' '}>
-          But minor variations are allowed (Recommended)
-        </p>
-      </button>
-      <button
-        onClick={() => textChange(2)}
-        className={
-          texts[2] === 'No' ? styles.negativeButtons : styles.ststatementButtons
-        }
-      >
-        <span
-          className={
-            texts[2] === 'No' ? styles.negativeText : styles.statementText
-          }
-        >
-          {texts[2]}
-        </span>
-        <p className={texts[2] === 'No' ? styles.negativeParText : ' '}>
-          I am only looking for a name, not a Domain
-        </p>
-      </button>
+      {buttonData.map((button, index) => (
+        <ButtonComponent key={index} {...button} />
+      ))}
     </div>
   )
 }
