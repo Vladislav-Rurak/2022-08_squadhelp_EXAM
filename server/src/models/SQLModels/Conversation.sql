571c912bce2bb73fe2aa359eef7d84c405acd642
Rural Vladislav <dolgkozly@gmail.com>
2023-11-13 15:11:48 +0200

    Alteration SQL models

--- server/src/models/SQLModels/Conversation.sql
+++ server/src/models/SQLModels/Conversation.sql
@@ -1,8 +1,11 @@
-CREATE TABLE Conversation{
+CREATE TABLE Conversations(
   id SERIAL PRIMARY KEY,
   participants INT[] NOT NULL,
   blackList BOOLEAN[] NOT NULL,
   favoriteList BOOLEAN[] NOT NULL,
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
-}
+  updated_at TIMESTAMP NOT NULL,
+  FOREIGN KEY (participants) REFERENCES "Users"(id),
+  FOREIGN KEY (conversation_id) REFERENCES "Messages"(id),
+  FOREIGN KEY (catalog_id) REFERENCES "Catalogs"(id)
+)
